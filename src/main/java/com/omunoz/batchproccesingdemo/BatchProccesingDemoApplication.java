94f2a4939198e8bbf4fb664beb559e6efded6ba7
Oscar Muñoz <cok.kike@gmail.com>
2023-03-09 16:39:15 -0600

    init

---
+++ src/main/java/com/omunoz/batchproccesingdemo/BatchProccesingDemoApplication.java
@@ -0,0 +1,13 @@
+package com.omunoz.batchproccesingdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BatchProccesingDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BatchProccesingDemoApplication.class, args);
+	}
+
+}
